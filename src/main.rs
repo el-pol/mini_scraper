81929c0f1637b2080e4b0b122014c33dd1236626
Pol Milian <pol.milian.dev@gmail.com>
2025-05-09 16:32:51 +0700

    use threads for multiple URLs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::thread;
+
 fn fetch_status(url: &str) -> Result<u16, Box<dyn std::error::Error>> {
     let resp = reqwest::blocking::get(url)?;
 
@@ -5,9 +7,20 @@ fn fetch_status(url: &str) -> Result<u16, Box<dyn std::error::Error>> {
 }
 
 fn main() {
-    let url = "https://www.rust-lang.org";
-    match fetch_status(url) {
-        Ok(status) => println!("Status code: {status}"),
-        Err(e) => eprintln!("Error: {e}"),
-    }
+    let multiple = vec![
+        "https://google.com",
+        "https://as.com",
+        "https://theverge.com",
+    ];
+
+    let handle = thread::spawn(|| {
+        for i in multiple {
+            match fetch_status(i) {
+                Ok(status) => println!("Status code: {status}"),
+                Err(e) => eprintln!("Error: {e}"),
+            }
+        }
+    });
+
+    handle.join().unwrap();
 }
