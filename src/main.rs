260711ae0fbf2fb353d180b2498fa2d31d279b3e
Pol Milian <pol.milian.dev@gmail.com>
2025-05-09 14:25:49 +0700

    return status code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+fn fetch_status(url: &str) -> Result<u16, Box<dyn std::error::Error>> {
+    let resp = reqwest::blocking::get(url)?;
+
+    Ok(resp.status().as_u16())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let url = "https://www.rust-lang.org";
+    match fetch_status(url) {
+        Ok(status) => println!("Status code: {status}"),
+        Err(e) => eprintln!("Error: {e}"),
+    }
 }
